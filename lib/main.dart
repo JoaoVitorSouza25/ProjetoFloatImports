913a897afa04c624fef8b988d0fc309623e3c366
João Vitor de Paula Souza <contato.joaovps@gmail.com>
2023-04-18 22:51:38 -0300

    Commit1804
    Criado Tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:appfloat/model/drawer.dart';
+import 'package:appfloat/view/login.dart';
 
 //teste1aaaaaaaaaaaaa
-void main() {
-  runApp(
-    MaterialApp(
+void main()
+{
+  runApp(const Inicializacao());
+}
+
+class Inicializacao extends StatelessWidget {
+  const Inicializacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Contatos',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Minha aplicação'),
-        ),
-        body: Center(
-          child: Text('Corpo da minha aplicação'),
-        ),
-        drawer: Drawerr(),
-      ),
-    ),
-  );
+      home: Login(),
+    );
+  }
 }
